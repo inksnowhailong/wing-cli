499395ec17e1317b0dceb23f65bb2ff936157f84
张海龙 <inksnowhailong@gmail.com>
2023-04-20 00:19:23 +0800

    '提交依赖修改'

--- core/gitCommand/lib/index.js
+++ core/gitCommand/lib/index.js
@@ -6,7 +6,6 @@ const log = require("@wing-cli/log");
 module.exports = gitCommand;
 
 function gitCommand(program) {
-  // const gitCommand = new commander.Command("git");
   program
     .command("Gpush [commit]")
     .description("一次性 拉取并推送远程库,出问题会报错")
@@ -43,17 +42,4 @@ function execFunc(codes) {
       execFunc(codes.slice(1));
     }
   );
-}
-async function checkGlobalUpdate(npmName, currentVersion) {
-  // 获取所有 版本号
-  const { getNpmSemverVersion } = require("@wing-cli/get-npm-info");
-  const lastVersion = await getNpmSemverVersion(currentVersion, npmName);
-  // 对比并提示更新
-  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
-    log.warn(
-      `当前最新版为${lastVersion}`,
-      `当前版本为${currentVersion},请手动更新到最新版本`
-    );
-    log.info("版本更新命令:", colors.yellow(`npm i -g ${npmName}`));
-  }
 }
--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -5,10 +5,14 @@ module.exports = {
   getNpmVersion,
   getNpmSemverVersion,
   getNpmVersion,
+  checkNpmVersion,
+  checkVersionCommand,
 };
 const axios = require("axios");
 const urljoinImp = import("url-join");
 const semver = require("semver");
+const readPkgUp = require("read-pkg-up");
+
 // 获取 npm包在npm网中的信息
 async function getNpmInfo(npmName, registry) {
   if (!npmName) return null;
@@ -60,4 +64,28 @@ async function getNpmSemverVersion(baseVersion, npmName, registry) {
   if (newVersions && newVersions.length > 0) {
     return newVersions[0];
   }
+}
+// 检查指定包的大于指定版本号的情况
+async function checkNpmVersion(npmName, currentVersion) {
+  // 获取所有 版本号
+  const lastVersion = await getNpmSemverVersion(currentVersion, npmName);
+  return lastVersion && semver.gt(lastVersion, currentVersion);
+  /*
+    log.warn(
+      `当前最新版为${lastVersion}`,
+      `当前版本为${currentVersion},请手动更新到最新版本`
+    );
+    log.info("版本更新命令:", colors.yellow(`npm i -g ${npmName}`));
+  */
+}
+async function checkVersionCommand(program) {
+  program
+    .command("checkNpmVersion")
+    .description("检查当前目录下的pkg 并选择要检查哪些包的更新")
+    .action(async () => {
+      (async () => {
+        console.log(await readPkgUp());
+      })();
+    })
+    .alias("cnv");
 }
